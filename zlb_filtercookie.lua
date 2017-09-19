79f2352f9e52514842c5ebc5982b1720d20071eb
shi7161979 <2541679@qq.com>
2017-09-19 23:14:39 +0800

    当Cookie X_GARY_TAG 指定的upstream 不存在时proxy到正式环境

--- zlb_filtercookie.lua
+++ zlb_filtercookie.lua
@@ -1,5 +1,15 @@
 local cjson = require("cjson")
 local servername = ngx.var.server_name;
+
+local function is_include(value, tbl)
+    for _,v in ipairs(tbl) do
+      if v == value then
+          return true
+      end
+    end
+    return false
+end
+
 local cookiefilter = {}
 if ckfilterpara ~= nil then
 	cookiefilter =  ckfilterpara[servername]
@@ -20,7 +30,14 @@ if cookiestr ~= nil then
     local i,j= string.find(cookiestr,"X_GRAY_TAG=([^;]+)") 
     if i ~= nil then
         local t = string.sub(cookiestr,i+string.len("X_GRAY_TAG="),j)       
-        ngx.var.vhost = t.."."..servername
+        local vhost = t.."."..servername
+        local upstream = require "ngx.upstream"
+        local get_servers = upstream.get_servers
+        local get_upstreams = upstream.get_upstreams
+        local us = get_upstreams()
+        if is_include(vhost,us) then  
+            ngx.var.vhost = vhost
+        end
     end
     ngx.log(ngx.ERR,"upstream Host:"..ngx.var.vhost..",".."cookie:"..ngx.var.vcookie)
 end
