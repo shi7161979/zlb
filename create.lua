4d1e2ef94c137f138e58cd709efd303753d5871f
root <shi7161979@gmail.com>
2017-11-26 23:25:52 +0800

    aa

--- create.lua
+++ create.lua
@@ -144,7 +144,7 @@ for domain,domainCfg in pairs(zlbcfg) do
        			ngx.say("      set $vhost   \""..upstream.."\";");
        			ngx.say("      set $vcookie \"\";");
         		ngx.say("      rewrite_by_lua_file /opt/zlb/zlb_filtercookie.lua;")
-       			ngx.say("      proxy_pass http://$vhost/;"); 
+       			ngx.say("      proxy_pass http://$vhost;"); 
                         ngx.say("      proxy_http_version 1.1;");
                         ngx.say("      proxy_set_header Host $host;");
        			ngx.say("      proxy_set_header Connection \"\";");
